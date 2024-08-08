8a640804ad3c09a61f1f7b1fa0322cb8ba43c4f0
Arnaldo Assis Ferreira <arnaldo_assisferreira@outlook.com>
2024-08-08 09:09:19 -0300

    postlist - form

--- src/components/PostList/index.tsx
+++ src/components/PostList/index.tsx
@@ -6,11 +6,11 @@ export function PostList() {
         <PostListContainer>
             <form action="">
                 <div>
-                    <h4>Pubicações</h4>
-                    <p>6 publicações</p>
+                    <h4 className='title-S'>Publicações</h4>
+                    <p className='text-S'>6 publicações</p>
 
                 </div>
-                <input type="text" />
+                <input type="text" placeholder='Buscar Conteúdo' />
             </form>
 
         </PostListContainer>
--- src/components/PostList/styles.ts
+++ src/components/PostList/styles.ts
@@ -4,4 +4,32 @@ export const PostListContainer = styled.div`
   display: flex;
   flex: 1;
   margin-top: 4.5rem;
+
+  form {
+    display: flex;
+    flex: 1;
+    flex-direction: column;
+  }
+
+  div {
+    display: flex;
+    flex: 1;
+    flex-direction: row;
+    justify-content: space-between;
+    margin-bottom: 0.75rem;
+  }
+
+  input {
+    background: ${(props) => props.theme["base-input"]};
+    color: ${(props) => props.theme["base-text"]};
+    padding: 0.75rem 1rem;
+    border-radius: 6px;
+    border: 1px solid ${(props) => props.theme["base-border"]};
+    font-family: "Nunito", sans-serif;
+    font-size: 1rem;
+
+    &:focus {
+      border: 1px solid ${(props) => props.theme["blue"]};
+    }
+  }
 `;
