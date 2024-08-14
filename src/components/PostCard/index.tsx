9a0844080b103fe8a9b3ebc206f79bd694122b8d
Arnaldo Assis Ferreira <arnaldo_assisferreira@outlook.com>
2024-08-14 08:43:29 -0300

    pagina de post com router param

--- src/components/PostCard/index.tsx
+++ src/components/PostCard/index.tsx
@@ -10,7 +10,7 @@ interface PostCardProps {
 export function PostCard(props: PostCardProps) {
     const { post } = props
     return (
-        <PostCardContainer to="/post">
+        <PostCardContainer to={`/post/${post.number}`}>
             <div>
                 <h4 className='title-S'>{post.title}</h4>
                 <p className='text-S'>{dateString(post.created_at)}</p>
--- src/pages/PostPage/index.tsx
+++ src/pages/PostPage/index.tsx
@@ -1,12 +1,15 @@
 
+import { useParams } from "react-router-dom";
 import { PostContent } from "../../components/PostContent";
 import { PostHeader } from "../../components/PostHeader";
 import { PostPageContainer } from "./styles";
 
 export function PostPage() {
+    const { postId } = useParams();
     return (
         <PostPageContainer>
             <PostHeader />
+            {postId}
             <PostContent />
         </PostPageContainer>
     )
